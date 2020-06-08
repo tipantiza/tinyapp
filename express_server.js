5d374736b647f1b4aa60d9e1c5c66238bf7a7d07
landon tipantiza <landontipantiza@gmail.com>
2020-06-08 23:06:51 +0000

    added some html

--- express_server.js
+++ express_server.js
@@ -15,3 +15,11 @@ app.listen(port, () => {
   console.log(`Example app listening on port: ${port}!`);
   
 })
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase)
+})
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World<b><body><html>\n")
+})
