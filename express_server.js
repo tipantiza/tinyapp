51ff60a703dbfe3b6fe164d83478e299bf60ed04
landon tipantiza <landontipantiza@gmail.com>
2020-06-09 00:11:32 +0000

    added views directory that conatins html file and contains some bootstrap stylesheet

--- express_server.js
+++ express_server.js
@@ -2,25 +2,32 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+app.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars)
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-})
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}!`);
   
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World<b><body><html>\n")
-})
+});
