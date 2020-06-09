710d6a9dfd5ad8e4d768b9b85bc9a4bae9dabaf3
landon tipantiza <landontipantiza@gmail.com>
2020-06-09 01:04:27 +0000

    created new pages for single urls and their shorted version

--- express_server.js
+++ express_server.js
@@ -8,6 +8,12 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+})
+
 app.get('/urls', (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars)
