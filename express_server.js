7643cfaa1e9b2b017107b184f5465560b403dcdb
landon tipantiza <landontipantiza@gmail.com>
2020-06-09 04:42:12 +0000

    eslinted express_server.js

--- express_server.js
+++ express_server.js
@@ -8,12 +8,12 @@ app.set('view engine', 'ejs');
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -22,15 +22,15 @@ app.get("/urls/new", (req, res) => {
 
 app.get('/urls', (req, res) => {
   let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World<b><body><html>\n")
+  res.send("<html><body>Hello <b>World<b><body><html>\n");
 });
 
 app.get("/", (req, res) => {
@@ -38,14 +38,14 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render("urls_show", templateVars)
-})
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}!`);
@@ -53,12 +53,12 @@ app.listen(port, () => {
 });
 
 
-function generateRandomString() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 6; i++ ) {
-       result += characters[Math.floor(Math.random() * charactersLength)];
-    }
-    return result;
-}
+const generateRandomString = () => {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters[Math.floor(Math.random() * charactersLength)];
+  }
+  return result;
+};
